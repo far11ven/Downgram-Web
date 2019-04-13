9cb8d189cb195c7425c2bbd17e80b5cb818cf149
far11ven <kushalbhalaik@gmail.com>
2019-04-13 20:58:05 +0530

    added "no-cors"

--- index.js
+++ index.js
@@ -6,8 +6,17 @@
 function getMedia() {
     var url = document.getElementById("search-box").value;
 
-    fetch('https://downgram-back-end.herokuapp.com/api/getmedia?link=https://www.instagram.com/p/BqDJ9LqgCMO/')
-  .then(response => console.log('response', response))
-  .catch(err=>console.log('err',err))
-    
+    fetch('https://downgram-back-end.herokuapp.com/api/getmedia?link=https://www.instagram.com/p/BqDJ9LqgCMO/', {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "no-cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers'": "*"
+        }
+    })
+        .then(response => console.log('response', response))
+        .catch(err => console.log('err', err))
+
 }
