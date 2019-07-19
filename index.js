341147ecabfe5a260259e52bb5ad9ea06e844e73
far11ven <kushalbhalaik@gmail.com>
2019-07-19 20:05:52 +0530

    with Dark Mode

--- index.js
+++ index.js
@@ -4,208 +4,243 @@ let url;
 let imageLinks = [];
 let videoLinks = [];
 
-window.onload = function () {
+// add + '&dl=1'
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const userName = urlParams.get('user');
+window.onload = function() {
+  changeTheme(localStorage.getItem("darkMode")); //select theme
+  const urlParams = new URLSearchParams(window.location.search);
+  const userName = urlParams.get("user");
 
-    console.log("user:", userName);
+  const searchQuery = urlParams.get("search");
 
-    if (!userName) {                  //if user is null
-        saveViewCount();
+  document.getElementById("search-box").value = searchQuery;
+  if (searchQuery) {
+    getMedia(searchQuery);
+  }
+
+  btnActivation(); //to make search button disabled by default if searchquery is empty
+
+  if (!userName) {
+    //if user is null
+    saveViewCount();
+  }
+
+  if (
+    window.location.pathname === "/" ||
+    window.location.pathname === "/index.html"
+  ) {
+    var dialogShownOn = localStorage.getItem("dialogShownOn");
+
+    if (dialogShownOn !== new Date().toLocaleDateString()) {
+      $("#StartUpModal").modal("show"); //display startup modal
+
+      var today = new Date().toLocaleDateString();
+      localStorage.setItem("dialogShownOn", today);
     }
 
-    console.log("path", window.location.pathname);
-    if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+    $("#spinner").show(); //shows loader
+    fetch("config.json")
+      .then(response => response.json())
+      .then(responseJSON => {
+        config = responseJSON;
+        getSessionCount();
+      });
+  } else if (window.location.pathname === "/pages/404.html") {
+    window.location.replace("https://www.downgram.in");
+  }
 
-        var dialogShownOn = localStorage.getItem('dialogShownOn')
-
-        if (dialogShownOn !== new Date().toLocaleDateString()) {
-
-            $('#StartUpModal').modal('show');  //display startup modal
-
-            var today = new Date().toLocaleDateString();
-            localStorage.setItem('dialogShownOn', today);
-        }
-
-        $("#spinner").show();  //shows loader
-        fetch("config.json")
-            .then(response => response.json())
-            .then(responseJSON => {
-                config = responseJSON;
-                getSessionCount();
-
-            });
-    } else if (window.location.pathname === '/pages/404.html') {
-
-        window.location.replace("https://www.downgram.in")
-
-    }
-
-    $("a[title~='Host']").hide();
+  $("a[title~='Host']").hide();
 };
 
-function saveViewCount() {
-
-    let sessionBody = { channelType: 'web' };
-
-    fetch('https://downgram-back-end.herokuapp.com/api/saveviewcount', {
-        method: "POST",
-        body: JSON.stringify(sessionBody),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(responseJson => {
-
-            console.log(responseJson);
-
-        })
-        .catch(err => {
-            console.log('err', err);
-
-        });
-
+function btnActivation() {
+  if (document.getElementById("search-box").value === "") {
+    document.getElementById("search-btn").disabled = true;
+  } else {
+    document.getElementById("search-btn").disabled = false;
+  }
 }
 
+function saveViewCount() {
+  let sessionBody = { channelType: "web" };
 
+  fetch("https://downgram-back-end.herokuapp.com/api/saveviewcount", {
+    method: "POST",
+    body: JSON.stringify(sessionBody),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(responseJson => {
+
+    })
+    .catch(err => {
+      console.log("err", err);
+    });
+}
 
 function getSessionCount() {
-    var url = document.getElementById("search-box").value;
-    $("a[title~='Host']").hide(); //hides 000webhost banner
+  var url = document.getElementById("search-box").value;
+  $("a[title~='Host']").hide(); //hides 000webhost banner
 
-    fetch('https://downgram-back-end.herokuapp.com/api/sessioncount')
-        .then(response => response.json())
-        .then(responseJson => {
-            let totalSessions = responseJson.result.$numberDouble;
+  fetch("https://downgram-back-end.herokuapp.com/api/sessioncount")
+    .then(response => response.json())
+    .then(responseJson => {
+      let totalSessions = responseJson.result.$numberDouble;
 
-            $(document).ready(function () {
-                console.log($("span.stats").text())
-                $("span.stats").text(totalSessions);
-            });
+      $(document).ready(function() {
+        $("span.stats").text(totalSessions);
+      });
 
-            $("#spinner").hide();  //hides loader
-
-        })
-        .catch(err => {
-            console.log('err', err);
-            $("#spinner").hide();  //hides loader
-        });
-
+      $("#spinner").hide(); //hides loader
+    })
+    .catch(err => {
+      console.log("err", err);
+      $("#spinner").hide(); //hides loader
+    });
 }
 
 function saveSessionDetails(url) {
-
-    let sessionBody = { linkURL: url, channelType: 'web' };
-
-    $("a[title~='Host']").hide(); //hides 000webhost banner
-
-    fetch('https://downgram-back-end.herokuapp.com/api/savesession', {
-        method: "POST",
-        body: JSON.stringify(sessionBody),
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(responseJson => {
-
-            console.log(responseJson);
-
-            $("#spinner").hide();  //hides loader
-
-        })
-        .catch(err => {
-            console.log('err', err);
-            $("#spinner").hide();  //hides loader
-        });
-
+  let sessionBody = { linkURL: url, channelType: 'web' };
+  $("a[title~='Host']").hide(); //hides 000webhost banner
+  fetch('https://downgram-back-end.herokuapp.com/api/savesession', {
+      method: "POST",
+      body: JSON.stringify(sessionBody),
+      headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+      }
+  })
+      .then(response => response.json())
+      .then(responseJson => {
+          $("#spinner").hide();  //hides loader
+      })
+      .catch(err => {
+          console.log('err', err);
+          $("#spinner").hide();  //hides loader
+      });
 }
 
-
-
-function getMedia() {
-    // remove attached items & start loader
-    $(document).ready(function () {
-        $("#errormessage").remove();
-        $("#downloadlink").empty();
-        $("#spinner").show(); //shows loader
-
+function getMedia(searchQuery) {
+  // remove attached items & start loader
+  $(document).ready(function() {
+    $("#errormessage").each(function() {
+      $(this).remove();
     });
+    $("#downloadlink").empty();
+    $("#spinner").show(); //shows loader
+  });
 
-    url = document.getElementById("search-box").value;
+  url = searchQuery;
 
-    fetch('https://downgram-back-end.herokuapp.com/api/getmedia?link=' + url)
-        .then(response => response.json())
-        .then(responseJson => {
+  fetch("https://downgram-back-end.herokuapp.com/api/getmedia?link=" + url)
+    .then(response => response.json())
+    .then(responseJson => {
+      if (responseJson.message == "Hello") {
+        imageLinks = responseJson.result.imagelinks;
+        videoLinks = responseJson.result.videolinks;
 
-            if (responseJson.message == "Hello") {
-
-                imageLinks = responseJson.result.imagelinks;
-                videoLinks = responseJson.result.videolinks;
-
-                console.log(imageLinks.length);
-                console.log(videoLinks.length);
-
-                $(document).ready(function () {
-                    for (var i = 0; i < imageLinks.length; i++)
-                        $("#downloadlink").append(`
-                      <div class="card border-secondary">
-                      <img id="itemimg" class="card-img-top" src="` + imageLinks[i] + `" />
+        $(document).ready(function() {
+        $("#downloadlink").append(`<h5>Available Downloads : <span id="downloadcount"> </span></h5>`)
+        $("#downloadCount").value = imageLinks.length + videoLinks.length;
+        
+        for (var i = 0; i < imageLinks.length; i++)
+            $("#downloadlink").append(
+              `
+                      <div class="card">
+                      <img id="itemimg" class="card-img-top" src="` +
+                imageLinks[i] +
+                `" />
                       <div class="card-body">
                         <h5 class="card-title"><i class="far fa-file-image"></i></h5>
-                        <a id="imgdownloadlink" class="card-link" href="` + imageLinks[i] + '&dl=1' + `" target="_blank">
+                        <a id="imgdownloadlink" class="card-link" href="` +
+                imageLinks[i] +
+                "&dl=1" +
+                `" target="_blank">
                           <u>click to download <i class="far fa-arrow-alt-circle-down"></i></u></a>
                       </div>
                     </div>
-                    `);
+                    `
+            );
 
-                    for (var j = 0; j < videoLinks.length; j++)
-                        $("#downloadlink").append(`
-                      <div class="card border-secondary">
-                      <video style="width: 100%;" src="` + videoLinks[j] + `" controls></video>
+          for (var j = 0; j < videoLinks.length; j++)
+            $("#downloadlink").append(
+              `
+                      <div class="card">
+                      <video style="width: 100%;" src="` +
+                videoLinks[j] +
+                `" controls></video>
                       <div class="card-body">
                         <h5 class="card-title"><i class="far fa-file-video"></i></h5>
-                        <a id="viddownloadlink" class="card-link" href="` + videoLinks[j] + '&dl=1' + `" target="_blank">
+                        <a id="viddownloadlink" class="card-link" href="` +
+                videoLinks[j] +
+                "&dl=1" +
+                `" target="_blank">
                           <u>click to download <i class="far fa-arrow-alt-circle-down"></i></u></a>
                       </div>
                     </div>
-                    `);
+                    `
+            );
+        });
 
-                });
+        //savesession details
+        saveSessionDetails(url);
+      } else if (
+        responseJson.message === "Please enter a valid INSTAGRAM link"
+      ) {
+        $(document).ready(function() {
+          $(".error").append(
+            `
+                      <span id="errormessage" class="message"><i class="fas fa-exclamation-triangle"></i> ` +
+              responseJson.message +
+              `</span>
+                    `
+          );
+        });
+      } else if (responseJson.message === "Please enter a valid link") {
+        $(document).ready(function() {
+          $(".error").append(
+            `
+                      <span id="errormessage" class="message"><i class="fas fa-exclamation-triangle"></i> ` +
+              responseJson.message +
+              `</span>
+                    `
+          );
+        });
+      }
+      $("#spinner").hide(); //hides loader
+    })
+    .catch(err => {
+      console.log("err", err);
+      $("#spinner").hide(); //hides loader
+      $(document).ready(function() {
+        $(".error").append(
+          `<span id="errormessage" class="message"><i class="fas fa-exclamation-triangle"></i> ` +
+            " Something went wrong! Please try again." +
+            `</span>`
+        );
+      });
+    });
+}
 
-                //savesession details
-                saveSessionDetails(url);
+function themeSelection() {
+  let isSelected = document.getElementById("theme-toggle").checked;
 
-            } else if (responseJson.message === "Please enter a valid INSTAGRAM link") {
+  localStorage.setItem("darkMode", !isSelected);
+   changeTheme(localStorage.getItem("darkMode"));
+}
 
-                $(document).ready(function () {
+function changeTheme(userPref) {
+  $(document).ready(function() {
+    if (userPref === "true") {
+      $("body").css("background-image", "url(assets/black_nature.jpg)");
+      $(".dark-th").css("color", "#ffffff");
+      $("#theme-toggle").prop('checked', true);
 
-                    $(".error").append(`
-                      <span id="errormessage" class="message"><i class="fas fa-exclamation-triangle"></i> ` + responseJson.message + `</span>
-                    `);
-                });
-
-
-            } else if (responseJson.message === "Please enter a valid link") {
-
-                $(document).ready(function () {
-                    $(".error").append(`
-                      <span id="errormessage" class="message"><i class="fas fa-exclamation-triangle"></i> ` + responseJson.message + `</span>
-                    `);
-
-                });
-            }
-            $("#spinner").hide();  //hides loader
-        })
-        .catch(err => {
-            console.log('err', err);
-            $("#spinner").hide();  //hides loader
-            $(document).ready(function () {
-                $(".error")
-                    .append(`<span id="errormessage" class="message"><i class="fas fa-exclamation-triangle"></i> ` + " Something went wrong! Please try again." + `</span>`);
-            });
-        })
+    } else {
+      $("body").css("background-image", "url(assets/white_nature.jpg)");
+       $(".dark-th").css("color", "rgba(0,0,0,.5)");
+       $("#theme-toggle").prop('checked', false);
+    }
+  });
 }
